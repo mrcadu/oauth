bb8aeb160094a5b2f115021c1e168aee850cd338
carlos <carlosvicc14@gmail.com>
2025-12-30 16:51:53 +0000

    Adding vercel file

---
+++ api/handler.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"net/http"
+	"oauth/api/v1/router"
+	"oauth/internal/model/datasource"
+	"oauth/locale"
+)
+
+var initialized bool
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	if !initialized {
+		locale.Setup()
+		datasource.Setup()
+		initialized = true
+	}
+
+	ginRouter := router.NewGin()
+	engine := ginRouter.CreateRouter()
+	engine.ServeHTTP(w, r)
+}
